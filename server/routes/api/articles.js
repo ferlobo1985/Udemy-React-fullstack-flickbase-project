6a710666efb9700a5206da5b824cab841dfdcadb
ferlobo1985 <fernando.lobo.prez@gmail.com>
2020-12-23 16:00:08 -0300

    Patch,delete,get article by id

--- server/routes/api/articles.js
+++ server/routes/api/articles.js
@@ -26,7 +26,50 @@ router.route('/admin/add_articles')
     } catch(error){
         res.status(400).json({message:'Error adding article',error: error })
     }
+});
+
+
+router.route("/admin/:id")
+.get(checkLoggedIn,grantAccess('readAny','article'),async(req,res)=>{
+    try{
+        const _id = req.params.id;
+        const article = await Article.findById(_id);
+        if(!article || article.length === 0){
+            return  res.status(400).json({message:'Article not found'});
+        }
+        res.status(200).json(article);
+    } catch(error){
+        res.status(400).json({message:'Error fetching article',error});
+    }
 })
+ .patch(checkLoggedIn,grantAccess('updateAny','article'),async(req,res)=>{
+     try{
+        const _id = req.params.id;
+        const article = await Article.findOneAndUpdate(
+            {_id},
+            {
+                "$set": req.body
+            },  
+            {
+                new:true
+            }
+        );
+        if(!article) return res.status(400).json({message:'Article not found'});
+        res.status(200).json(article);
+    } catch(error){
+        res.status(400).json({message:'Error updating article',error});
+    }
+})
+.delete(checkLoggedIn,grantAccess('deleteAny','article'),async(req,res)=>{
+    try{
+        const _id = req.params.id;
+        const article = await Article.findByIdAndRemove(_id);
+        if(!article) return res.status(400).json({message:'Article not found'});
+        res.status(200).json({_id:article._id});
+    } catch(error){
+        res.status(400).json({message:'Error deleting',error});
+    }
+});
 
 
 
