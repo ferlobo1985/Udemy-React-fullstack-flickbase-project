ad5aec841d6f49c764b3815bde5695609e20db4b
ferlobo1985 <fernando.lobo.prez@gmail.com>
2020-12-23 15:08:46 -0300

    Articles model

--- server/routes/api/articles.js
+++ server/routes/api/articles.js
@@ -6,6 +6,12 @@ const { grantAccess } = require('../../middleware/roles');
 // model 
 const { Article } = require('../../models/article_model');
 
+// add single article
+// admin get,patch,delete single article (draft or public)
+// get articles no auth
+// fetch articles load more
+// fetch articles, with pagination
+
 
 
 
