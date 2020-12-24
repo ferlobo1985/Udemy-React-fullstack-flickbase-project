47041d7c1ba90a50f539bc1a99d18d76a7fbeedb
ferlobo1985 <fernando.lobo.prez@gmail.com>
2020-12-24 00:28:19 -0300

    finishing sidedrawer

--- client/src/components/navigation/sideNavigation.js
+++ client/src/components/navigation/sideNavigation.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React,{ useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 
 import {
     Drawer,
@@ -16,22 +17,49 @@ import VpnKeyIcon from '@material-ui/icons/VpnKey';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 
 const SideDrawer = () => {
+    const [state,setState] = useState(false);
+
     return(
         <>
             <DehazeIcon
                 className="drawer_btn"
-                onClick={()=> alert('open')}
+                onClick={()=> setState(true)}
             />
-            <Drawer anchor={'right'} open={true} onClose={()=> alert('close')}>
+            <Drawer anchor={'right'} open={state} onClose={()=> setState(false)}>
 
                 <form style={{margin:'20px'}}>
                     <TextField id="outlined-basic" label="Search movie"
                     variant="outlined"
                     />
                 </form>
+                <Divider/>
+                <List>
+                    <ListItem button component={RouterLink} to="/" onClick={()=>setState(false)}>
+                        <ListItemIcon><HomeIcon/></ListItemIcon>
+                        <ListItemText primary="Home"/>
+                    </ListItem>
+                    <ListItem button component={RouterLink} to="/contact" onClick={()=>setState(false)}>
+                        <ListItemIcon><MailIcon/></ListItemIcon>
+                        <ListItemText primary="Contact"/>
+                    </ListItem>
 
 
-
+                    <ListItem button component={RouterLink} to="/auth" onClick={()=>setState(false)}>
+                        <ListItemIcon><VpnKeyIcon/></ListItemIcon>
+                        <ListItemText primary="Sign in"/>
+                    </ListItem>
+                    <ListItem button component={RouterLink} to="/auth" onClick={()=>setState(false)}>
+                        <ListItemIcon><VpnKeyIcon/></ListItemIcon>
+                        <ListItemText primary="Sign out"/>
+                    </ListItem>
+                </List>
+                <Divider/>
+                <List>
+                    <ListItem button component={RouterLink} to="/dashboard" onClick={()=>setState(false)}>
+                        <ListItemIcon><DashboardIcon/></ListItemIcon>
+                        <ListItemText primary="Dashboard"/>
+                    </ListItem>
+                </List>
             </Drawer>
         </>
     )
