6cc15e7928727671efe28cf283e79c03e35926c4
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-02-22 23:25:39 -0300

    Creatting the email service

--- server/config/email.js
+++ server/config/email.js
@@ -6,16 +6,46 @@ let transporter = nodemailer.createTransport({
     service:"Gmail",
     secure: true,
     auth:{
-        user: 'email',
-        pass:'pass'
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD
     }
 });
 
 const contactMail = async(contact) => {
     try{
+        let mailGenerator = new Mailgen({
+            theme:"default",
+            product:{
+                name: "Flickbase",
+                link: `${process.env.EMAIL_MAIN_URL}`
+            }
+        });
+        
+        const email = {
+            body:{
+                intro:[
+                    'Someone sent you a message',
+                    `Email: ${contact.email}`,
+                    `Firstname: ${contact.firstname}`,
+                    `Lastname: ${contact.lastname}`
+                ],
+                outro: [ `${contact.message}`]
+            }
+        };
 
+        let emailBody = mailGenerator.generate(email);
+        let message = {
+            from: process.env.EMAIL,
+            to:process.env.EMAIL, 
+            subject:"Contact",
+            html:emailBody
+        }
+
+
+        await transporter.sendMail(message);
+        return true
     } catch(error){
-
+        if(error) throw error;
     }
 }
 
