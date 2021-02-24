579a227fa231b21f57f511043a92767eb4aa41cf
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-02-24 10:19:40 -0300

    Creating the categories layout

---
+++ client/src/components/dashboard/categories/addCategory.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useDispatch } from 'react-redux'
+
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+import {
+    TextField,
+    Button
+} from "@material-ui/core"
+
+const AddCategories = () => {
+    const dispatch = useDispatch();
+
+    const formik = useFormik({
+        enableReinitialize:true,
+        initialValues: {name:''},
+        validationSchema: Yup.object({
+            name:Yup.string()
+            .required('The name is required')
+        }),
+        onSubmit:(values, { resetForm })=>{
+            //// go to the server 
+            resetForm();
+        }
+    })
+
+    const errorHelper = (formik, values) => ({
+        error: formik.errors[values] && formik.touched[values] ? true:false,
+        helperText: formik.errors[values] && formik.touched[values] ? formik.errors[values] : null
+    });
+
+    return(
+        <>
+            <form onSubmit={formik.handleSubmit}>
+                <div className="form-group">
+                    <TextField
+                        style={{width:'100%'}}
+                        name="name"
+                        label="Enter a name"
+                        variant="outlined"
+                        {...formik.getFieldProps('name')}
+                        {...errorHelper(formik,'name')}
+                    />
+                </div>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                >
+                    Add category
+                </Button>
+
+            </form>
+        </>
+    )
+
+}
+
+export default  AddCategories
--- client/src/components/dashboard/categories/index.js
+++ client/src/components/dashboard/categories/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import AdminLayout from '../../../hoc/adminLayout';
-
+import AddCategories from './addCategory'
 import { useDispatch, useSelector } from 'react-redux'
+
 
 import { Table, Row, Col } from 'react-bootstrap';
 
@@ -12,7 +13,24 @@ const Categories = () => {
         <AdminLayout section="Categories">
             <Row>
                 <Col>
-                
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Name of category</td>
+                            </tr>
+                            <tr>
+                                <td>Name of category</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </Col>
+                <Col>
+                    <AddCategories/>
                 </Col>
             </Row>
         </AdminLayout>
